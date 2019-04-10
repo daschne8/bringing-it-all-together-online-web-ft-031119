796e636c3ca3d01483af623f44196f0e3e8fab0b
Daniel Schneider <daschne8@gmail.com>
2019-04-10 18:42:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,7 @@ class Dog
   end
 
   def self.find_or_create_by(name:,breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, AND breed = ?",name,breed)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?",name,breed)
     if !song.empty?
       dog_data = dog[0]
       dog = self.new_from_db(dog_data)
