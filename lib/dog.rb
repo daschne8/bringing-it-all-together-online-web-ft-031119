d473611939082d884c797eeca4a82e871a0008e3
Daniel Schneider <daschne8@gmail.com>
2019-04-10 18:18:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+  attr_accessor :name,:breed
+  attr_reader :id
 
+  def initialize(id=nil,name,breed)
+    @id,@name,@breed = id,name,breed
+  end
+
+  def save
+
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs(
+
+    )
+    DB[:conn].execute("")
+  end
 end
